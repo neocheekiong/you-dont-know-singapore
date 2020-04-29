4729d15c185844306ee29df2a19dfc6441f4a55e
neocheekiong <neocheekiong@gmail.com>
2020-04-29 19:41:28 +0800

    Map layout done

---
+++ questions.js
@@ -0,0 +1,5 @@
+First HDB
+correct 1.2973869, 103.8030340 45 stirling road
+wrong 1.2861369, 103.8330279 1 tiong bahru
+wrong 1.3885466, 103.9878045 1 changi village
+wrong 1.3394303, 103.8534428 1 toa Payoh Lorong 7
