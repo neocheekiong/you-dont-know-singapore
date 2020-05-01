128acca3e92000676e6ded6ffad90f5b3ba20b21
neocheekiong <neocheekiong@gmail.com>
2020-05-01 21:36:43 +0800

    Now displays questions and answers.

--- js/quiz.js
+++ js/quiz.js
@@ -16,16 +16,23 @@ class Question {
      * @param {Number=1} score 
      * Total score to be given for this question
      */
-    constructor(question, answerOptions, feedback, score = 1) {
+    constructor(question, coordinates, answerOptions, score = 1) {
         this.question = question;
+        this.coordinates = coordinates
         this.options = answerOptions;
-        this.feedback = feedback;
         this.score = score;
+        this.feedback = {
+            correct: `That's correct! The answer is ${this.options.find((answer)=>answer.fraction===1)}`
+        };
     }
     checkAnswer (answer) {
-        let questionScore = this.options.reduce((score, option)=>{
-            if(answer === option.text) return score + this.score * option.fraction;
+        let questionScore = this.options.reduce((score, answerOption)=>{
+            console.log(`score:${score}`)
+            console.log(answerOption)
+            if(answer === answerOption.answerDescription) return score + this.score * answerOption.fraction;
+            return score;
         }, 0);
+        console.log(questionScore)
         return mapQuizApp.player.score + questionScore;
     }
 }
@@ -45,10 +52,70 @@ class AnswerOption {
 }
 
 const mapQuizApp = {
-    player: {
-        name: 'You',
-        score: 0,
-    },
     questions: [],
     selectedAnswers: [],
+    player: {
+        name: "Player",
+        score: 0,
+        currentQuestion: 0,
+    },
+    
+    getQuestions (questions) {
+        questions.forEach(question => {
+            mapQuizApp.questions.push(new Question(question.question,question.coordinates, question.answerOptions))
+        });
+    },
+
+    renderCurrentQuestion(){
+        const currentQuestion = this.questions[this.player.currentQuestion];
+        $('#questions').text(currentQuestion.question)
+        this.renderAnswers();
+        $('#panel').append(this.renderSubmitButton());
+    },
+
+    renderAnswers(){
+        const $answerContainer = $('#answers');
+        const currentQuestion = this.questions[this.player.currentQuestion];
+        currentQuestion.options.forEach((answerOption)=>{
+            if(answerOption.coordinates){
+                L.marker([answerOption.coordinates.latitude, answerOption.coordinates.longitude]).addTo(map);
+            }
+            const answerDescription = answerOption.answerDescription
+            const $answer = createRadioButton(answerDescription);
+            const $label = createLabel(answerDescription)
+            $answerContainer.append($answer, $label);
+        })
+    },
+
+    renderSubmitButton(){
+        return $('<button>').text('Submit').on('click', ()=>{
+            const submittedAnswer = getAnswer();
+            console.log(submittedAnswer);
+            mapQuizApp.player.score = mapQuizApp.questions[mapQuizApp.player.currentQuestion].checkAnswer(submittedAnswer);  
+        })
+    }
+
 }
+
+function createRadioButton(value) {
+    return $('<input>').attr({
+        type: 'radio',
+        name: 'answer',
+        value: value
+    })
+}
+
+function createLabel(value) {
+    return $('<label>').attr({
+        for: value
+    }).text(value)
+}
+
+function getAnswer() {
+    return $('input:radio[name=answer]:checked').val();
+}
+
+$(function () {
+    mapQuizApp.getQuestions(questions);
+    mapQuizApp.renderCurrentQuestion();
+});
