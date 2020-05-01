38e75f7cf07a4e2483515bf63e56da589e0743d4
neocheekiong <neocheekiong@gmail.com>
2020-05-01 22:14:51 +0800

    tidying up map.js

--- js/map.js
+++ js/map.js
@@ -2,12 +2,12 @@ const bottomLeft = [1.162859, 103.574099];
 const topRight = [1.478801, 104.103136];
 const bounds = L.bounds(bottomLeft,topRight)
 const center = bounds.getCenter();
-//center = [1.337104, 103.845851]
+
 const map = L.map('map').setView([center.x, center.y], 12);
+
 map.setMaxBounds([
     topRight,
     bottomLeft,
-
 ]);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmVvY2hlZWtpb25nIiwiYSI6ImNrOW1wemluYzA5ZTYzbHBqbjU4MWs1dzQifQ.ziIGnkXPUoaOEvk9FrMgbQ', {
