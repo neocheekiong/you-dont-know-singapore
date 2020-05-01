58e186f80ac7534457bfc132601de419fd39bfdc
neocheekiong <neocheekiong@gmail.com>
2020-05-01 12:18:26 +0800

    Singapore Regions Layer added. Need to separate them to create different objects for each layer

--- map.js
+++ map.js
@@ -10,10 +10,12 @@ map.setMaxBounds([
 
 ]);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmVvY2hlZWtpb25nIiwiYSI6ImNrOW1wemluYzA5ZTYzbHBqbjU4MWs1dzQifQ.ziIGnkXPUoaOEvk9FrMgbQ', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/satellite-streets-v11',
+    id: 'mapbox/outdoors-v11',
     tileSize: 512,
     zoomOffset: -1
 }).addTo(map);
+
+omnivore.kml(`regions/MP14_REGION_NO_SEA_PL.kml`).addTo(map);
